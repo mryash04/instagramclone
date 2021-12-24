3c1d8264ca4a63edbbde9eb02de60104b61f6296
mryash04 <yash.04may@gmail.com>
2021-12-24 23:57:22 +0530

    code update

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../../models/Post");
+const User = require("../../models/Users");
 const auth = require("../../middleware/auth");
 
+// get all post 
+router.get("/allpost", async(req, res) =>{
+    try{
+        let post = await Post.find().populate("postedBy", "name email");
+
+        res.status(200).json({post});
+    }catch(err){
+        console.log(err.mesaage);
+        res.status(400).send("Server Error");
+    }
+});
+
+//create a post
 router.post("/createPost", auth, async(req, res) =>{
     const{title, body} = req.body;
 
@@ -10,30 +24,56 @@ router.post("/createPost", auth, async(req, res) =>{
         return res.status(400).send("Please fill the details properly");
     }
 
+    const user = await User.findById(req.user.user.id).select("-password")
+
     let post = new Post({
         title,
         body,
-        postedBy : req.user.user.id
+        postedBy : user
     });
 
     await post.save();
-    console.log(post)
     console.log("This is user", req.user.user);
     res.status(200).json({post});
 });
 
+//mypost
+router.get("/mypost", auth, async(req, res) =>{
+    console.log("This is my post id", {postedBy : req.user})
+    try{
+        let post = await Post.find({postedBy : req.user.user.id});
+
+        res.status(200).json({post});
+    }catch(err){
+        console.log(err.message);
+        res.status(400).send("Server Error");
+    }
+})
+
+// get single post by id
+router.get("/post/:id", async(req, res) =>{
+    try{
+        const id = req.params.id
+        let post = await Post.findById(id).populate("postedBy", "name email");
+
+        res.status(200).json({post});
+    }catch(err){
+        console.log(err.mesaage);
+        res.status(400).send("Server Error");
+    }
+});
+
+// delete single post by id
+router.delete("/post/:id", async(req, res) =>{
+    try{
+        const id =req.params.id
+        let post = await Post.findByIdAndDelete(id)
+
+        res.status(200).json({success:true,message:"deleted SuccessFully"});
+    }catch(err){
+        console.log(err.mesaage);
+        res.status(400).send("Server Error");
+    }
+})
+
 module.exports = router;
-
-// https://www.youtube.com/watch?v=PRACd1gm8m0&list=PLB97yPrFwo5g0FQr4rqImKa55F_aPiQWk&index=13
-
-// "title": "title 1",
-// "body": "This is post",
-// "image": "No Photo",
-// "postedBy": "61c365a126a8687c50b8eb1f",
-// "date": "2021-12-24T16:54:20.122Z",
-// "_id": "61c5fb41aab6f731902afbc3",
-// "__v": 0
-
-// const token = useSelector<RootState, boolean>(
-//     state => state.register.token
-// );
